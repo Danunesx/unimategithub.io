6118e476b26416705a4ec3bd7ec2f81b7a0cced9
Carlos Jr <carlosrogeriocr02@gmail.com>
2021-08-20 14:29:44 -0300

    remove rotate of slider

--- scripts/slider.js
+++ scripts/slider.js
@@ -4,10 +4,10 @@ const swiper = new Swiper('.swiper', {
     centeredSlides: true,
     slidesPerView: 'auto',
     coverflowEffect: {
-        rotate: 30,
+        rotate: 0,
         stretch: 0,
         depth: 100,
-        modifier: 2,
+        modifier: 1,
         slideShadows: false,
     },
     pagination: {
